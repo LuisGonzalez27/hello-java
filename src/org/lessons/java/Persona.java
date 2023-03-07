de2d7fcc2ffc931847e5018ac42f60afb79990b9
LuisGonzalez27 <luis27coding@gmail.com>
2023-03-07 17:49:06 +0100

    Nuova classe Persona

---
+++ src/org/lessons/java/Persona.java
@@ -0,0 +1,16 @@
+package org.lessons.java;
+
+public class Persona {
+    String nome;
+    String cognome;
+    int eta;
+    String colorePreferito;
+
+    //costruttore
+    Persona(String nome, String cognome, int eta, String colorePreferito){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.eta = eta;
+        this.colorePreferito = colorePreferito;
+    }
+}
--- src/org/lessons/java/StampaSaluto.java
+++ src/org/lessons/java/StampaSaluto.java
@@ -2,5 +2,15 @@ package org.lessons.java;
 public class StampaSaluto{
     public static void main(String[] args) {
         System.out.println("Hello Java!");
+
+        //BONUS
+        //creo due oggetti diversi, con due istanze diverse pero della stessa classe Persona
+        Persona persona1 = new Persona("Luca", "Rossi", 25 , "Blu");
+        Persona persona2 = new Persona("Silvia", "Ferrari", 27 , "Rosso");
+
+        System.out.println(persona1.nome + " " + persona1.cognome);
+        System.out.println(persona2.nome + " " + persona2.cognome);
+
+
     }
 }
